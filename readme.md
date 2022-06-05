b6b879bbc576fab0bbf4f6b6cb1bb8db611ccdf7
Anuj Maurya <anujmaurya563400@gmail.com>
2022-06-05 07:25:51 +0530

    Final Commit

---
+++ readme.md
@@ -0,0 +1,27 @@
+# Bootcamp API
+
+> Backend API for Gramoday App.
+
+## Usage
+
+Please go through the documentation link https://documenter.getpostman.com/view/17758184/Uz5GpGej
+
+## Install Dependencies
+
+```
+npm install
+```
+
+## Run App
+
+```
+# Run in dev mode
+npm run dev
+
+# Run in prod mode
+npm start
+```
+
+### Version: 1.0.0
+
+### Licence: AMI
--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -5,14 +5,38 @@ const {
   createBootcamp,
   updateBootcamp,
   deleteBootcamp,
+  getBootcampsInRadius,
+  bootcampPhotoUpload,
 } = require('../controllers/bootcamps');
+
+const Bootcamp = require('../models/Bootcamp');
+const advancedResults = require('../middleware/advancedResults');
+
+// Include other resource routers
+const courseRouter = require('./courses');
+const reviewRouter = require('./reviews');
+
 const router = express.Router();
 
-router.route('/').get(getBootcamps).post(createBootcamp);
+const { protect, authorize } = require('../middleware/auth');
+
+// Re-route into other resource routers
+router.use('/:bootcampId/courses', courseRouter);
+router.use('/:bootcampId/reviews', reviewRouter);
+
+router
+  .route('/')
+  .get(advancedResults(Bootcamp, 'courses'), getBootcamps)
+  .post(protect, authorize('publisher', 'admin'), createBootcamp);
 router
   .route('/:id')
   .get(getBootcamp)
-  .put(updateBootcamp)
-  .delete(deleteBootcamp);
+  .put(protect, authorize('publisher', 'admin'), updateBootcamp)
+  .delete(protect, authorize('publisher', 'admin'), deleteBootcamp);
+router.route('/radius/:zipcode/:distance').get(getBootcampsInRadius);
+
+router
+  .route('/:id/photo')
+  .put(protect, authorize('publisher', 'admin'), bootcampPhotoUpload);
 
 module.exports = router;
